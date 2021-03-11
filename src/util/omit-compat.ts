78b9c7a7c87c71ba6126b5522333a805dea8982c
Tim <timkrins@gmail.com>
2021-03-11 14:14:10 +0000

    fix: fix the maximum stack size exceeded by using different Omit helper

--- src/util/omit-compat.ts
+++ src/util/omit-compat.ts
@@ -1,6 +1,5 @@
 // export type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
 export type DiffCompat<T extends string, U extends string> = ({ [P in T]: P } &
   { [P in U]: never } & { [x: string]: never })[T]
-export type OmitCompat<T, K> = {
-  [P in DiffCompat<Extract<keyof T, string>, Extract<keyof K, string>>]: T[P]
-}
+// for TS < 3.5
+export type OmitCompat<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>
